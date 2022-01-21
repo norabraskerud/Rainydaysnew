43a9dac265390c02fc6beed3c53f0b2f6c0c48ab
norabraskerud <norabraskerud@gmail.com>
2022-01-21 17:28:51 +0100

    ad

--- js/women.js
+++ js/women.js
@@ -22,13 +22,15 @@ callApi();
 
 function createHtml(results){
     for (let i = 0; i < results.length; i++) {
-        console.log(results[i].images.src);
+        console.log(results[i]);
+        console.log(results[i].images[0].src);
+        
         
 
         resultsContainer.innerHTML +=   `<a href="details.html?id=${results[i].id}" class="card">                                                  
                                         <h1 class="name">${results[i].name}</h1>
                                         <div class="results-image" 
-                                         style="background-image: url('${results[i].images.src}')"></div>
+                                         style="background-image: url('${results[i].images[0].src}')"></div>
                                         <h3 class="price">${results[i].prices.price}</h3>
                                         <button class="select">${results[i].add_to_cart.text}</button>
                                         </a>`;
